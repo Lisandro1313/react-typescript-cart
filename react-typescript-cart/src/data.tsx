b4913ce6e7d95ccf724f17abfd08a87f908098a6
Lisandro Etcheverry <70907207+Lisandro1313@users.noreply.github.com>
2023-01-27 11:48:05 -0300

    Add files via upload

---
+++ react-typescript-cart/src/data.tsx
@@ -0,0 +1,27 @@
+const data = {
+    products: [
+      {
+        id: "1",
+        name: "Box Surprise",
+        price: 2000,
+        image:
+          "https://s3-alpha-sig.figma.com/img/b2e0/3c9c/847b48db2f906f8399df8c91e7664c96?Expires=1675641600&Signature=E9FkqOFmcd59shTgjAVis4upt2zywqERYFbhllwBeJeoSJaCFj3DsVMBqm5FTrFa9bbjMV4HsjsmY6fCV7U~gggCWQ-fhU2JQ1jqz-3I5UDaQF-ZFc9Gt9QHYErUr9~~Fa-X-EpxViXDGdp0HtP0z6s9PCabiomHrlE~zE8uilY-tuuFF2k0Cc7Rxv6UabqWTPfm6vngGnuc0MiNjnxKRVm~MFdTWaoogRSptquzqYukbNswHUeMC~sbVRJDun~DfNc0Sjsc~Qe1WIV79-FPleQgyIDj1PMbYOeqbwxib767QbEXLGpmH3xxcEgUFl3jvqUCyMsit5Me1GXyopvMig__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+      },
+      {
+        id: "2",
+        name: "T Shirt",
+        price: 800,
+        image:
+          "https://s3-alpha-sig.figma.com/img/4e05/3671/4aa327c3748b597e2a1c9fb9cc75cb07?Expires=1675641600&Signature=ngs-~YVpfzVO2rzivcISI34wmnDWM~iJ1LyBHxxd7qcLx2m~c4hkWk1avuV0cINl7gZhZCn3hWF-O-0ayvfHigbRerANcRBtyXx8ybDUxl9Ur9MO6hQgcvr1iY76yqae7tRURJ4qmN53Sc-3z10CsIjFLYNztsL8DKYX~Cr1e7Q9Gp1DLyukeoM4DYzaHnch8UBzYwv~JDg9FvCmBuKWxyjC6CG8hfJjdaUXw~XuWiZqlemCYvMZsmN4F-gLYASk-7ynCjYg-~5Btcuh6z~Lb2C8NwycB6arasL9yz-cJEP8WrZf1FS21D8wizjtXjkBP48~9NCjIzfTRMZ5ZauJCA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+      },
+      {
+        id: "3",
+        name: "Water Bottle",
+        price: 1000,
+        image:
+          "https://s3-alpha-sig.figma.com/img/cc82/ff7b/02a852df43c6795f632bc179a38fd85c?Expires=1675641600&Signature=MatWb0RdkCeLNpRsG5akfi0KhJpql4z2lucGAr~6zvEP7TZHv9nQ-yJxhUl914gZUhLpyUu-ei~eVtw09c0GlX4TiAPGPZm3w59JQD~be3rB272OG9INTzBK0tfhDBFHeLq9Ddfkvswk2Gsin~n8JVQGjUFCG9~-CjVe5G5xMqMq~4oIqRpEcvuX53NQigkRrFqnpjRK4fTjAH6bXTfmiVv6z-3DjROR9D96Zj-S8Na3T0TB3dY0so8mj3JvcuFq2pyKj6OdcU~JKUzLR8bCCJjlTfwQNAuWhOcmug5NJWHAmX9AUQy97Mk20Q54wc2wnbAS4awN4vg-wJAZMVQjOA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+      }
+    ]
+  };
+  export default data;
+  
